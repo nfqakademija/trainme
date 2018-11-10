437fa2399d2433f783c642eae046b741483a771f
Ignas Dailydė <ignas.dailyde@gmail.com>
2018-11-11 01:19:28 +0200

    #5 Update and create fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,9 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\AvailabilitySlot;
+use App\Entity\ScheduledWorkout;
+use App\Entity\Tag;
 use App\Entity\Trainer;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -10,13 +13,121 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        for ($i = 1; $i < 20; $i++) {
+        $availabilitySlots = [
+            [
+                'from' => '2018-11-10 10:00:00',
+                'to' => '2018-11-10 12:00:00',
+                'trainerId' => 1
+            ],
+            [
+                'from' => '2018-11-10 14:00:00',
+                'to' => '2018-11-10 16:00:00',
+                'trainerId' => 1
+            ],
+            [
+                'from' => '2018-11-10 13:00:00',
+                'to' => '2018-11-10 15:00:00',
+                'trainerId' => 2
+            ],
+            [
+                'from' => '2018-11-10 09:00:00',
+                'to' => '2018-11-10 17:00:00',
+                'trainerId' => 3
+            ]
+        ];
+
+        $scheduledWorkouts = [
+            [
+                'from' => '2018-11-10 10:00:00',
+                'to' => '2018-11-10 10:30:00',
+                'trainerId' => 1
+            ],
+            [
+                'from' => '2018-11-10 11:00:00',
+                'to' => '2018-11-10 12:00:00',
+                'trainerId' => 1
+            ],
+            [
+                'from' => '2018-11-10 14:00:00',
+                'to' => '2018-11-10 15:00:00',
+                'trainerId' => 1
+            ],
+            [
+                'from' => '2018-11-10 15:00:00',
+                'to' => '2018-11-10 16:00:00',
+                'trainerId' => 1
+            ],
+            [
+                'from' => '2018-11-10 12:00:00',
+                'to' => '2018-11-10 13:00:00',
+                'trainerId' => 2
+            ],
+            [
+                'from' => '2018-11-10 10:00:00',
+                'to' => '2018-11-10 14:00:00',
+                'trainerId' => 3
+            ],
+            [
+                'from' => '2018-11-10 14:00:00',
+                'to' => '2018-11-10 16:00:00',
+                'trainerId' => 3
+            ]
+        ];
+        $tags = [
+            'Tag 1' => 'Tag 1 description',
+            'Tag 2' => 'Tag 2 description',
+            'Tag 3' => 'Tag 3 description'
+        ];
+
+        $tagObjects = [];
+
+        foreach ($tags as $name => $description) {
+            $tag = new Tag();
+            $tag->setName($name);
+            $tag->setDescription($name);
+            $tagObjects[] = $tag;
+            $manager->persist($tag);
+        }
+
+        for ($i = 0; $i < 20; $i++) {
             $trainer = new Trainer();
-            $trainer->setName('John Doe');
+            $trainer->setName(sprintf("John Doe - %s", $i));
             $trainer->setPersonalStatement("personal statment");
             $trainer->setPhone("8686868686");
+            $trainer->setImageUrl("https://placeimg.com/123/480/animals");
+            $this->addReference(sprintf("Trainer %s", $i + 1), $trainer);
             $manager->persist($trainer);
         }
+
+        $trainer1 = $this->getReference(sprintf("Trainer %s", 1));
+        $trainer2 = $this->getReference(sprintf("Trainer %s", 4));
+        $trainer3 = $this->getReference(sprintf("Trainer %s", 8));
+
+        $trainer1->addTag($tagObjects[0]);
+        $trainer1->addTag($tagObjects[2]);
+        $trainer2->addTag($tagObjects[1]);
+        $trainer3->addTag($tagObjects[0]);
+        $trainer3->addTag($tagObjects[1]);
+        $trainer3->addTag($tagObjects[2]);
+
+
+        foreach ($availabilitySlots as $availabSl) {
+            $availabilitySlot = new AvailabilitySlot();
+            $availabilitySlot->setStartsAt(new \DateTime($availabSl['from']));
+            $availabilitySlot->setEndsAt(new \DateTime($availabSl['to']));
+
+            $availabilitySlot->setTrainer($this->getReference(sprintf("Trainer %s", $availabSl['trainerId'])));
+            $manager->persist($availabilitySlot);
+        }
+
+        foreach ($scheduledWorkouts as $schedWork) {
+            $scheduledWorkout = new ScheduledWorkout();
+            $scheduledWorkout->setStartsAt(new \DateTime($schedWork['from']));
+            $scheduledWorkout->setEndsAt(new \DateTime($schedWork['to']));
+            $scheduledWorkout->setTrainer($this->getReference(sprintf("Trainer %s", $schedWork['trainerId'])));
+            $manager->persist($scheduledWorkout);
+        }
+
         $manager->flush();
     }
 }
