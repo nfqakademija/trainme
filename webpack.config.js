fff22f90e83198f653663dcedd4e2542ba1735fd
Gintautas Plonis <gplonis@gmail.com>
2018-11-19 13:23:42 +0000

    #14 Enable React.

--- webpack.config.js
+++ webpack.config.js
@@ -38,6 +38,9 @@ Encore
     // enables Sass/SCSS support
     .enableSassLoader()
 
+    //enable React support
+    .enableReactPreset()
+
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
 
