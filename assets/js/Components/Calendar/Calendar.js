225ae56ef248d4d49ab08bd009b43bd94b64164e
Gintautas Plonis <gplonis@gmail.com>
2018-11-19 17:14:41 +0000

    #14 Code react calendar component.

---
+++ assets/js/Components/Calendar/Calendar.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import BigCalendar from 'react-big-calendar';
+import moment from 'moment';
+
+const localizer = BigCalendar.momentLocalizer(moment);
+
+const Calendar = (props) => (
+    <React.Fragment>
+        <BigCalendar
+            localizer={localizer}
+            views={['month', 'week', 'day']}
+            events={[]}
+        />
+    </React.Fragment>
+);
+
+export default Calendar;
