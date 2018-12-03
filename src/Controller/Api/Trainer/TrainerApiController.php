99925b1ac9726a363411224136c0a54afe88f0a5
Ignas Dailydė <ignas.dailyde@gmail.com>
2018-12-03 10:52:20 +0200

    #43 Trainer profile API

---
+++ src/Controller/Api/Trainer/TrainerApiController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller\Api\Trainer;
+
+
+use App\Entity\User;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class TrainerApiController extends AbstractController
+{
+    /**
+     * @Route("/api/trainer", methods={"PUT"})
+     * @param Request $request
+     * @return JsonResponse
+     */
+    public function updateAction (Request $request) {
+        try {
+            $user = $this->getUser();
+
+            $data = json_decode($request->getContent(), true);
+
+            if (!$user instanceof User) {
+                throw new \Exception('User expected');
+            }
+
+            $trainer = $user->getTrainer();
+
+            if (isset($data['personal_statement'])) {
+                $trainer->setPersonalStatement($data['personal_statement']);
+            }
+
+            if (isset($data['phone'])) {
+                $trainer->setPhone($data['phone']);
+            }
+
+            if (isset($data['location'])) {
+                $trainer->setLocation($data['location']);
+            }
+
+            return new JsonResponse($trainer);
+
+        } catch (\Throwable $e) {
+            return new JsonResponse($e->getMessage(), 400);
+        }
+    }
+}
