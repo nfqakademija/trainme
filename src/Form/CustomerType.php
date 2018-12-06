986d2349d95a6255625601eebe1058277c8388d7
Gintautas Plonis <gplonis@gmail.com>
2018-12-06 20:08:36 +0000

    #49 Style customer registration form.

--- src/Form/CustomerType.php
+++ src/Form/CustomerType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Customer;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -13,8 +14,8 @@ class CustomerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-            ->add('phone', TelType::class);
+            ->add('name', TextType::class, ['attr' => ['class' => 'regInput']])
+            ->add('phone', TelType::class, ['attr' => ['class' => 'regInput']]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
